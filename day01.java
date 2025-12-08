eab783c69181f1758707c02ff7acf2cefaa5b18c
Kav1-rajan <kavirajan.kr58@gmail.com>
2025-12-08 15:40:17 +0530

    Commit message

---
+++ day01.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Main{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+String s1 = sc.nextLine();
+String s2 = sc.nextLine();
+int count = UserMainCode.countOccurances(s1,s2);
+System.out.println(count);
+}
+}
+class UserMainCode{
+public static countOccurances(string s1 , string s2){
+return 0;
+}
+}
